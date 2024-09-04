605c1f579eec00d6f56bd2c84a8a90691c71bfec
Fasalu Rahman <fadilrifna@gmail.com>
2024-09-04 17:07:56 +0530

    TypeScript

---
+++ Arrow.js
@@ -0,0 +1,8 @@
+// var GetValue=function(){
+//     return 10
+// }
+// console.log(GetValue());
+var GetArrow = function (m) {
+    return 10 * m;
+};
+console.log(GetArrow(2));
---
+++ Arrow.ts
@@ -0,0 +1,9 @@
+// var GetValue=function(){
+//     return 10
+// }
+// console.log(GetValue());
+
+const GetArrow=(m)=>{
+    return 10*m
+}
+console.log(GetArrow(2));
